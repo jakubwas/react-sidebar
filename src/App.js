1087952e2963982220a2f8409d4594747986befb
jakubwas <jakubaws@gmail.com>
2020-12-27 13:55:40 +0100

    Updates menu list

--- src/App.js
+++ src/App.js
@@ -9,22 +9,73 @@ const App = () => {
   const sidebarHeader = 'PSS';
 
   const menuItems = [
-    { name: 'Home', to: '/', icon: 'home', subMenuItems: [] },
     { name: 'Reporting Panel', to: '/reportingpanel', icon: 'columns', subMenuItems: [] },
-    { name: 'Company', to: '/company', icon: 'building', 
+    { name: 'Analysis', to: '/analysis', icon: 'columns', subMenuItems: [] },
+    { name: 'Company', to: '', icon: 'columns', 
       subMenuItems: [
-        { name: 'Plants', to: '/plants' },
+        { name: 'Devices Groups', to: '/devicesgroups' },
         { name: 'Devices', to: '/devices' },
-        { name: 'Devices Groups', to: '/devicesgroups'}
-      ] },
-    { name: 'Accidents', to: '/accidents', icon: 'times', subMenuItems: [] },
-    { name: 'Production', to: '/production', icon: 'building', subMenuItems: [] },
-    { name: 'Setting', to: '/settings', icon: 'cog', 
-    subMenuItems: [
-      { name: 'Users', to: '/plants' },
-      { name: 'Languages', to: '/devices' },
-      { name: 'Translation', to: '/devicesgroups'}
-    ]}
+        { name: 'Plants', to: '/plants' },
+        { name: 'Shifts', to: '/shifts' },
+        { name: 'Teams', to: '/teams' },
+        { name: 'Lines', to: '/lines' },
+        { name: 'Employees', to: '/employees' }
+      ]
+    },
+    { name: 'Production', to: '', icon: 'columns', 
+      subMenuItems: [
+        { name: 'Recipes', to: '/recipes' },
+        { name: 'Recipes Groups', to: '/recipesgroups' },
+        { name: 'Production', to: '/production' },
+        { name: 'Production Groups', to: '/productiongroups' },
+        { name: 'Production Types', to: '/productiontypes' },
+      ] 
+    },
+    { name: 'Wastes', to: '', icon: 'columns', 
+      subMenuItems: [
+        { name: 'Wastes List', to: '/wasteslist' },
+        { name: 'Wastes Groups', to: '/wastesgroups' },
+        { name: 'Wastes Types', to: '/wastestypes'},
+        { name: 'Wastes Execution', to: '/wastesexecution' }
+      ]
+    },
+    { name: 'Measurements', to: '', icon: 'columns', 
+      subMenuItems: [
+        { name: 'Measurements List', to: '/measurementslist' },
+        { name: 'Measurements Set', to: '/measurementsset' }
+      ]
+    },
+    { name: 'Checkings', to: '', icon: 'columns', 
+      subMenuItems: [
+        { name: 'Checkings List', to: '/checkingslist' },
+        { name: 'Checkings Set', to: '/checkingsset' }
+      ]
+    },
+    { name: 'Accidents', to: '', icon: 'columns', 
+      subMenuItems: [
+        { name: 'Accidents List', to: '/accidentslist' },
+        { name: 'Accidents Groups', to: '/accidentsgroups' },
+        { name: 'Accidents Types', to: '/accidentstypes' }
+      ]
+    },
+    { name: 'Products', to: '', icon: 'columns', 
+      subMenuItems: [
+        { name: 'Products List', to: '/productslist' },
+        { name: 'Products Groups', to: '/productsgroups' }
+      ]
+    },
+    { name: 'Settings', to: '', icon: 'columns', 
+      subMenuItems: [
+        { name: 'Hosts', to: '/hosts' },
+        { name: 'Languages', to: '/languages' },
+        { name: 'Messages', to: '/messages' },
+        { name: 'Permissions', to: '/permissions' },
+        { name: 'Roles', to: '/roles' },
+        { name: 'Translations', to: '/translations' },
+        { name: 'Units', to: '/units' },
+        { name: 'Users', to: '/users' }
+      ]
+    },
   ];
 
   return (
